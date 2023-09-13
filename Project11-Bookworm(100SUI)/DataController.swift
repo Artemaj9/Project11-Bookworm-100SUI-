21b9ca205e8b7a9df70ed351b357058dcb63a9c5
Artem <moiseev@physics.msu.ru>
2023-09-13 16:38:08 +0300

    Add managed object context, button for generate and save random student

---
+++ Project11-Bookworm(100SUI)/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  Project11-Bookworm(100SUI)
+//
+//  Created by Artem on 13.09.2023.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Bookworm")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
--- Project11-Bookworm(100SUI)/Project11_Bookworm_100SUI_App.swift
+++ Project11-Bookworm(100SUI)/Project11_Bookworm_100SUI_App.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct Project11_Bookworm_100SUI_App: App {
+    @StateObject private var dataController = DataController()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
         }
     }
 }
