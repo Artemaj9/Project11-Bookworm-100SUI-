2a3c682862fca2448b39c312404c17572d0b125c
Artem <moiseev@physics.msu.ru>
2023-09-13 15:12:32 +0300

    Initial Commit

---
+++ Project11-Bookworm(100SUI)/Project11_Bookworm_100SUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Project11_Bookworm_100SUI_App.swift
+//  Project11-Bookworm(100SUI)
+//
+//  Created by Artem on 13.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Project11_Bookworm_100SUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
