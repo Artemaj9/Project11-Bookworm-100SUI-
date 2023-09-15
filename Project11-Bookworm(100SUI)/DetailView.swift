40e56bdea87f312f227d8d1b2fb4c22d1a583de7
Artem <moiseev@physics.msu.ru>
2023-09-15 11:02:22 +0300

    add .onDelete, delete alert

--- Project11-Bookworm(100SUI)/DetailView.swift
+++ Project11-Bookworm(100SUI)/DetailView.swift
@@ -9,6 +9,9 @@ import CoreData
 import SwiftUI
 
 struct DetailView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
+    @State private var showingDeleteAlert = false
     
     let book: Book
     var body: some View {
@@ -39,6 +42,27 @@ struct DetailView: View {
         }
         .navigationTitle(book.title ?? "Unknown Book")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Delete book", isPresented: $showingDeleteAlert) {
+            Button("Delete", role: .destructive, action: deleteBook)
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Are you sure?")
+        }
+        .toolbar {
+            Button {
+                showingDeleteAlert = true
+            } label: {
+                Label("Delete this book", systemImage: "trash")
+            }
+        }
+    }
+    
+    
+    func deleteBook() {
+        moc.delete(book)
+        // uncomment this line if you want to make the deletion permanent
+        // try? moc.save()
+        dismiss()
     }
 }
 
